aa464b6af4f3ca7a0be114cfa70c88f626e2a0d2
Sverre Paulsen <sverrepaulsen@Sverres-MacBook-Pro.local>
2020-03-30 11:09:38 +0200

    movement commit

--- game.js
+++ game.js
@@ -1,11 +1,110 @@
-var canvas = document.querySelector('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+var context, controller, rectangle, loop;
 
-var c = canvas.getContext('2d');
-c.fillStyle = "red";
-c.fillRect(100, 855, 40, 40);
-c.fillStyle = "#fff000";
-c.fillRect(600, 800, 30, 100);
-c.fillRect(800, 700, 30, 200);
-c.fillRect(1000, 600, 30, 300);
+context = document.querySelector("canvas").getContext("2d");
+
+context.canvas.height = window.innerHeight - 40;
+context.canvas.width = window.innerWidth - 20;
+height = context.canvas.height;
+width = context.canvas.width;
+
+rectangle = {
+
+    height:32,
+    jumping:true,
+    width:32,
+    x:144, // center of the canvas
+    x_velocity:0,
+    y:0,
+    y_velocity:0
+  
+  };
+
+  controller = {
+
+    left:false,
+    right:false,
+    up:false,
+    keyListener:function(event) {
+  
+      var key_state = (event.type == "keydown")?true:false;
+  
+      switch(event.keyCode) {
+  
+        case 37:// left key
+          controller.left = key_state;
+        break;
+        case 38:// up key
+          controller.up = key_state;
+        break;
+        case 39:// right key
+          controller.right = key_state;
+        break;
+  
+      }
+  
+    }
+  
+  };
+
+  loop = function() {
+
+    if (controller.up && rectangle.jumping == false) {
+  
+      rectangle.y_velocity -= 20;
+      rectangle.jumping = true;
+  
+    }
+
+    if (controller.left) {
+
+        rectangle.x_velocity -= 0.5;
+    
+      }
+    
+      if (controller.right) {
+    
+        rectangle.x_velocity += 0.5;
+    
+      }
+
+      rectangle.y_velocity += 1.5;// gravity
+      rectangle.x += rectangle.x_velocity;
+      rectangle.y += rectangle.y_velocity;
+      rectangle.x_velocity *= 0.9;// friction
+      rectangle.y_velocity *= 0.9;// friction
+
+    // if rectangle is falling below floor line
+    if (rectangle.y > height - 32) {
+
+        rectangle.jumping = false;
+        rectangle.y = height - 32;
+        rectangle.y_velocity = 0;
+
+  }
+
+    // if rectangle is going off the left of the screen
+    if (rectangle.x < -32) {
+
+        rectangle.x = width;
+
+    } 
+    else if (rectangle.x > width) {// if rectangle goes past right boundary
+
+        rectangle.x = -32;
+
+  }
+    context.fillStyle = "rgb(118,146,186)";
+    context.fillRect(0, 0, context.canvas.width, context.canvas.height);// x, y, width, height
+    context.fillStyle = "#ff0000";// hex for red
+    context.beginPath();
+    context.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+    context.fill();
+    
+    // call update when the browser is ready to draw again
+    window.requestAnimationFrame(loop);
+
+};
+
+window.addEventListener("keydown", controller.keyListener)
+window.addEventListener("keyup", controller.keyListener);
+window.requestAnimationFrame(loop);
